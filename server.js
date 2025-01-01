327d5ccd96f9b12b77d4d2901fd31fd0949b6fa7
Reyhan Maulana <144551736+Rerey155-del@users.noreply.github.com>
2025-01-01 22:13:37 +0700

    uiji

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const db = require('./db'); 
 const app = express();
 const port = process.env.PORT || 5000;
-
 const cors = require('cors');
+
 app.use(cors());
 
 // Middleware untuk parsing JSON
